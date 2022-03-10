7a415b35f0679c6877278bcc5aa40ad7a5cb01d0
Kisavi <deniskisavi@gmail.com>
2022-03-10 15:29:55 +0300

    Ft - add jquery to toggle images and description text

---
+++ js/file.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".set1").click(function () {
+        $(".description1").toggle();
+        $(".img1").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".set2").click(function () {
+        $(".description2").toggle();
+        $(".img2").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".set3").click(function () {
+        $(".description3").toggle();
+        $(".img3").toggle();
+    });
+});
