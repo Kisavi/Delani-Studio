a250ce19f1b23d63d7172ed600ebc91d7aec785c
Kisavi <deniskisavi@gmail.com>
2022-03-10 18:27:00 +0300

    Ft - alert user upon form submission

--- js/file.js
+++ js/file.js
@@ -1,3 +1,5 @@
+// WHAT WE DO SECTION //
+// toggling between images and description 
 $(document).ready(function () {
     $(".set1").click(function () {
         $(".description1").toggle();
@@ -5,16 +7,30 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $(".set2").click(function () {
-        $(".description2").toggle();
-        $(".img2").toggle();
-    });
-});
+// $(document).ready(function () {
+//     $(".set2").click(function () {
+//         $(".description2").toggle();
+//         $(".img2").toggle();
+//     });
+// });
 
-$(document).ready(function () {
-    $(".set3").click(function () {
-        $(".description3").toggle();
-        $(".img3").toggle();
-    });
-});
+// $(document).ready(function () {
+//     $(".set3").click(function () {
+//         $(".description3").toggle();
+//         $(".img3").toggle();
+//     });
+// });
+
+// PORTFOLIO SECTION //
+// CONTACT US SECTION //
+$(document).ready(function() {
+    $("#myForm form").submit(function(event) {
+        let name1Input = $("input#name1").val();
+        let name2Input = $("input#name2").val();
+        let userMessageInput = $("input#userMessage").val();
+        if(name1Input && name2Input && userMessageInput !== "") {
+            alert(`${name1Input} ${name2Input} we have received your message. Thank you for reaching out to us.`)
+        }
+       event.preventDefault();
+    })
+})
