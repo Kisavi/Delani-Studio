ea17232fbaa18c0c7cece844045ed2412bc4848d
Kisavi <deniskisavi@gmail.com>
2022-03-11 12:27:09 +0300

    update the toggling property in what we do section

--- js/file.js
+++ js/file.js
@@ -1,30 +1,42 @@
 // WHAT WE DO SECTION //
 // toggling between images and description 
-$(document).ready(function () {
-    $(".set1").click(function () {
-        $(".description1").toggle();
-        $(".img1").toggle();
+$(document).ready(function(){
+    $(".img1").click(function(){
+      $(".img1").hide('1500').hide('1000');
+      $(".description1").show('1500');
     });
-});
+    $(".description1").click(function(){
+      $(".description1").slideUp('1500');
+      $(".img1").show('1500');
+    });
+  });
 
-// $(document).ready(function () {
-//     $(".set2").click(function () {
-//         $(".description2").toggle();
-//         $(".img2").toggle();
-//     });
-// });
+  $(document).ready(function(){
+    $(".img2").click(function(){
+      $(".img2").hide('1500').hide('1000');
+      $(".description2").show('1500');
+    });
+    $(".description2").click(function(){
+      $(".description2").slideUp('1500');
+      $(".img2").show('1500');
+    });
+  });
 
-// $(document).ready(function () {
-//     $(".set3").click(function () {
-//         $(".description3").toggle();
-//         $(".img3").toggle();
-//     });
-// });
+  $(document).ready(function(){
+    $(".img3").click(function(){
+      $(".img3").hide('1500').hide('1000');
+      $(".description3").show('1500');
+    });
+    $(".description3").click(function(){
+      $(".description3").slideUp('1500');
+      $(".img3").show('1500');
+    });
+  });
 
 // PORTFOLIO SECTION //
 // CONTACT US SECTION //
 $(document).ready(function() {
-    $("#myForm form").submit(function(event) {
+    $("form").submit(function(event) {
         let nameInput = $("input#userName").val();
         let emailInput = $("input#userEmail").val();
         let userMessageInput = $("input#userMessage").val();
